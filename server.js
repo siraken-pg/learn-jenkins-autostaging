a069a319573dc1df7ae98030d6903edd09482b0f
Kento Shirasawa <shirasawa.kento@gmail.com>
2024-01-05 21:02:55 +0900

    feat: try

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('This is from 00001 Example!');
 });
 
 app.listen(8080, () => {
