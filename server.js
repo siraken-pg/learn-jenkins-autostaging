311a31ecb7678971c38030de91e62f709ee6b180
Kento Shirasawa <shirasawa.kento@gmail.com>
2024-01-05 23:19:53 +0900

    add: listen info

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const app = express();
 
+const PORT = 8080;
+
 app.get('/', (req, res) => {
   // res.send('Hello! This is from 00001 Example.');
   res.render('Hello! This is from 00001 Example.');
 });
 
-app.listen(8080, () => {
-  console.log('Listening')
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`)
 });
