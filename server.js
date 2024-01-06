187f34330de9e51243dee2b2db2ae4860aa089b1
Kento Shirasawa <shirasawa.kento@gmail.com>
2024-01-07 02:21:50 +0900

    add: 00002

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+	res.send("Hello World! from 00002");
 });
 
 app.listen(8080, () => {
-  console.log('Listening')
+	console.log("Listening");
 });
