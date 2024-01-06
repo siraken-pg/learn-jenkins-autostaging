5b405e25a398048548e0f4c979e9696e766244b1
Kento Shirasawa <shirasawa.kento@gmail.com>
2024-01-07 01:51:30 +0900

    fix: use res.send

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 const PORT = 18080;
 
-app.get('/', (req, res) => {
-  // res.send('Hello! This is from 00001 Example.');
-  res.render('Hello! This is from 00001 Example.');
+app.get("/", (req, res) => {
+  console.log(`Request from ${req.ip}`);
+  res.send("Hello! This is from 00001 Example.");
 });
 
 app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`)
+  console.log(`Listening on http://localhost:${PORT}`);
 });
